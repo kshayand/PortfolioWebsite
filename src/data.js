9cc172fb47f66293f1b3e5e248b5101f40534be8
Akshaya Anand <32972936+kshayand@users.noreply.github.com>
2022-12-28 11:51:18 -0500

    minor changes to data

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ let timelineElements = [
     {
         id:1,
         title: "Masters Student",
-        location: "University of Maryland, College Park", 
+        location: "University of Maryland College Park", 
         description: "First year student in the UMD Machine Learning Masters Program.",
         skillIcons: [],
         date: "September 2022 - Present",
@@ -14,7 +14,7 @@ let timelineElements = [
     {
         id:2,
         title: "Co-founder",
-        location: "Korion Health Pittsburgh, PA", 
+        location: "Korion Health", 
         description: "Co-founder of a healthcare-tech company called Korion Health. We are developing a stethoscope and guided user interface to empower individuals to record their own heart and lung sounds.",
         link : <a href = "https://www.korionhealth.com/" target="_blank" rel="noopener noreferrer">Check us out<br /></a>,
         skillIcons: [SVGicon.html5, SVGicon.js, SVGicon.react],
@@ -25,29 +25,37 @@ let timelineElements = [
     {
         id:3,
         title: "Independent Contractor",
-        location: "5Metis Durham, NC ", 
-        description: "Automating data processing pipelines",
+        location: "5Metis", 
+        description: "Automating data processing pipelines.",
         skillIcons: [SVGicon.python],
         date: "March 2022 - Present",
         icon: "work"
 
     },
-    
     {
         id:4,
+        title: "Dance Teacher",
+        location: "Jayamangala School of Music and Dance", 
+        description: "Teaching Classical Indian dance.",
+        skillIcons: [],
+        date: "September 2019 - Present",
+        icon: "school"
+    },
+    {
+        id:5,
         title: "Technica Hackathon Award",
-        location: "University of Maryland, College Park", 
-        description: "Poseify - A web application to create stick-figure gifs from live video",
-        link : <a href = "https://devpost.com/software/poseify" target="_blank" rel="noopener noreferrer">Check out on Devpost<br /></a>,
+        location: "University of Maryland College Park", 
+        description: "Poseify - A web application to create stick-figure gifs from live video.",
+        link : <div><a href = "https://devpost.com/software/poseify" target="_blank" rel="noopener noreferrer">Check out on Devpost<br/></a><a href = "https://editor.p5js.org/kshayand/sketches/8TIDJ8xyI" target="_blank" rel="noopener noreferrer">Try Demo<br></br></a></div>,
         skillIcons: [SVGicon.html5, SVGicon.js],
         date: "November 2022",
         icon: "award"
 
     }, 
     {
-        id:5,
+        id:6,
         title: "Automated 5 phase classification of CT scans",
-        location: "The National Institutes of Health, Bethesda", 
+        location: "The National Institutes of Health", 
         description: "Automated classification of arterial, portal venous, nephrographic, delayed, and non-contrast phases for axial CT. Accepted as oral presentation at SPIE Medical Imaging 2023.",
         skillIcons: [SVGicon.python],
         date: "September 2022",
@@ -55,7 +63,7 @@ let timelineElements = [
 
     }, 
     {
-        id:6,
+        id:7,
         title: "Treehacks Hackathon Award",
         location: "Stanford University", 
         description: "EmergeNeed - An app to provide tools to use during a medical emergency.",
@@ -65,9 +73,9 @@ let timelineElements = [
         icon: "award"
     },
     {
-        id:7,
+        id:8,
         title: "Technica Hackathon Award",
-        location: "University of Maryland, College Park", 
+        location: "University of Maryland College Park", 
         description: "SheConnect - An app to connect women+ in tech through coffee chats.",
         link : <a href = "https://devpost.com/software/sheconnect" target="_blank" rel="noopener noreferrer">Check out on Devpost<br /></a>,
         skillIcons: [SVGicon.python,SVGicon.react, SVGicon.figma],
@@ -76,29 +84,29 @@ let timelineElements = [
 
     },
     {
-        id:8,
+        id:9,
         title: "Rare Variant Analysis and Risk Classification",
-        location: "The National Institutes of Health, Bethesda", 
-        description: "Conducted rare variant analysis of GRASP cohort and identified variant of interest. Used Random Forest Classifier to identify significant features from a large dataset of genes, TCR-B sequences, and clinical information",
+        location: "The National Institutes of Health", 
+        description: "Conducted rare variant analysis using genomic data. Used Random Forest Classifier to identify significant features from a large dataset of genes, TCR-B sequences, and clinical information.",
         skillIcons: [SVGicon.python, SVGicon.r],
         date: "September 2021",
         icon: "work"
 
     },
     {
-        id:9,
+        id:10,
         title: "Teaching Assistant",
-        location: "FAES, The National Institutes of Health", 
-        description: "Introduction to python",
+        location: "FAES-NIH", 
+        description: "Introduction to python.",
         skillIcons: [],
         date: "March 2021",
         icon: "school"
 
     },
     {
-        id:10,
+        id:11,
         title: "Bachelors Dual-Degree",
-        location: "University of Maryland, College Park", 
+        location: "University of Maryland College Park", 
         description: "Graduated with dual-degree in Computer Science and Biology.",
         skillIcons: [],
         date: "December 2019",
@@ -106,9 +114,9 @@ let timelineElements = [
 
     },
     {
-        id:11,
+        id:12,
         title: "Independent Contractor",
-        location: "AgriMetis Lutherville, MD", 
+        location: "AgriMetis", 
         description: "Set up database and visualizations for agri-chemical efficacy data. Automated the data processing pipeline.",
         skillIcons: [SVGicon.python],
         date: "September 2019",
@@ -116,29 +124,29 @@ let timelineElements = [
 
     },
     {
-        id:12,
+        id:13,
         title: "Teaching Assistant",
-        location: "University of Maryland, College Park", 
-        description: "Bioinformatic Algorithms",
+        location: "University of Maryland College Park", 
+        description: "Bioinformatic Algorithms.",
         skillIcons: [],
         date: "May 2019",
         icon: "school"
 
     },
     {
-        id:13,
+        id:14,
         title: "Teaching Assistant",
-        location: "University of Maryland, College Park", 
-        description: "Introductory Web Development",
+        location: "University of Maryland College Park", 
+        description: "Introductory Web Development.",
         skillIcons: [],
         date: "December 2018",
         icon: "school"
 
     },
     {
-        id:14,
+        id:15,
         title: "Bitcamp Hackathon Award",
-        location: "University of Maryland, College Park", 
+        location: "University of Maryland College Park", 
         description: "CloseCall - An Android app to detect a car crash and notify first responders.",
         link : <a href = "https://devpost.com/software/close-call" target="_blank" rel="noopener noreferrer">Check out on Devpost<br /></a>,
         skillIcons: [SVGicon.android,SVGicon.js],
